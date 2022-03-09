c34962ec13abfcc9da11386189d41899bae3545a
206 <206@academicos.cesde.edu.co>
2022-03-09 09:56:56 -0500

    a

--- js/llenadoTienda.js
+++ js/llenadoTienda.js
@@ -1,8 +1,16 @@
 //creando un arreglo de objetos
 
 let productos = [
-    {nombre:"batimovil", precio:400000, descripcion:"Batimovil a escala"},
-    {nombre:"baticamiseta", precio:60000, descripcion:"Camiseta original DC"}
+    {foto: 'img/papolin.jpg', nombre:"batimovil", precio:400000, descripcion:"Batimovil a escala"},
+    {foto: 'img/papolin2.jpg', nombre:"baticamiseta", precio:60000, descripcion:"Camiseta original DC"},
+    {foto: 'img/papolin.jpg', nombre:"batimovil", precio:400000, descripcion:"Batimovil a escala"},
+    {foto: 'img/papolin2.jpg', nombre:"baticamiseta", precio:60000, descripcion:"Camiseta original DC"},
+    {foto: 'img/papolin.jpg', nombre:"batimovil", precio:400000, descripcion:"Batimovil a escala"},
+    {foto: 'img/papolin2.jpg', nombre:"baticamiseta", precio:60000, descripcion:"Camiseta original DC"},
+    {foto: 'img/papolin.jpg', nombre:"batimovil", precio:400000, descripcion:"Batimovil a escala"},
+    {foto: 'img/papolin2.jpg', nombre:"baticamiseta", precio:60000, descripcion:"Camiseta original DC"},
+    {foto: 'img/papolin.jpg', nombre:"batimovil", precio:400000, descripcion:"Batimovil a escala"},
+    {foto: 'img/papolin2.jpg', nombre:"baticamiseta", precio:60000, descripcion:"Camiseta original DC"}
 
 ]
 
@@ -10,6 +18,24 @@ console.log(productos)
 
 //Recorrer un arreglo
 
+let fila = document.getElementById("tarjetas")
+
 productos.forEach(function(producto){
     console.log(producto.nombre)
+
+
+let columna = document.createElement("div")
+columna.classList.add("col")
+
+let tarjeta = document.createElement("div")
+tarjeta.classList.add("card")
+tarjeta.classList.add("h-100")
+
+let imagen = document.createElement("img")
+imagen.classList.add("card-img-top")
+imagen.src=producto.foto
+
+tarjeta.appendChild(imagen)
+columna.appendChild(tarjeta)
+fila.appendChild(columna)
 })
